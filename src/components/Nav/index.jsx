8347147a8d283e00513b95e32a9932a47fdc1a6b
ivan <ivanlisichkin@icloud.com>
2023-12-21 00:01:02 +0400

    feat:add svg

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,26 +2,18 @@ import React, { useContext } from 'react';
 import styles from './Nav.module.css';
 import NavigationLink from './NavigationLink';
 import { RouterContext } from '../../Router';
-
+import Calendar from '../../assets/icons/calendar.svg?react';
+import Sun from '../../assets/icons/sun.svg?react';
+import List from '../../assets/icons/list.svg?react';
+import Stack from '../../assets/icons/stack.svg?react';
+import Circle from '../../assets/icons/circle.svg?react';
 function Nav() {
   const { currentPath } = useContext(RouterContext);
   console.log('Current route:', currentPath);
   return (
     <aside className={styles.nav}>
       <div className={styles.logo}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-          />
-        </svg>
+        <Calendar />
         <h1>To Do App</h1>
       </div>
       <ul className={styles.nav__list}>
@@ -29,19 +21,7 @@ function Nav() {
           <NavigationLink
             to="/"
             styles={currentPath === '/' ? styles.activeLink : styles.unActiveLink}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-              />
-            </svg>
+            <List />
             <span>Все</span>
           </NavigationLink>
         </li>
@@ -50,19 +30,7 @@ function Nav() {
           <NavigationLink
             to="/today"
             styles={currentPath === '/today' ? styles.activeLink : styles.unActiveLink}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-              />
-            </svg>
+            <Sun />
             <span>Сегодня</span>
           </NavigationLink>
         </li>
@@ -70,19 +38,7 @@ function Nav() {
           <NavigationLink
             to="/categories"
             styles={currentPath === '/categories' ? styles.activeLink : styles.unActiveLink}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
-              />
-            </svg>
+            <Stack />
             <span>Категории</span>
           </NavigationLink>
         </li>
@@ -90,19 +46,7 @@ function Nav() {
           <NavigationLink
             to="/complited"
             styles={currentPath === '/complited' ? styles.activeLink : styles.unActiveLink}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
+            <Circle />
             <span>Завершенные</span>
           </NavigationLink>
         </li>
