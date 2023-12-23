d1847ec6034dd75fb54a9381462ad38acf1dbd86
ivan <ivanlisichkin@icloud.com>
2023-12-23 19:27:30 +0400

    fix:change config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-
+import path from 'path';
 export default defineConfig({
   plugins: [react({ runtime: 'automatic' }), svgr()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // используем path.resolve
+        sw: path.resolve(__dirname, 'src/pages/Home/sw.js'), // и здесь
+      },
+    },
+  },
 });
