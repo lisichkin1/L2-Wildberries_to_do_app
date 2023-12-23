f42656387e87b034dd19f2d7d57299298308028b
ivan <ivanlisichkin@icloud.com>
2023-12-23 15:13:37 +0400

    feat:add menu state

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../Nav';
 import styles from './Layout.module.css';
 function Layout({ children }) {
+  const [displayMenuMobile, setDisplayMenuMobile] = useState(true);
+
   return (
     <div className={styles.container}>
-      <Nav />
+      <Nav displayMenuMobile={displayMenuMobile} setDisplayMenuMobile={setDisplayMenuMobile} />
       <div className={styles.children}>{children}</div>
     </div>
   );
