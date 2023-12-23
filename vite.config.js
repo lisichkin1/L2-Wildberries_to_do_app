6cd3bbae977551236707af879664f3b2eb311448
ivan <ivanlisichkin@icloud.com>
2023-12-23 17:04:46 +0400

    feat:change config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react({ runtime: 'automatic' }), svgr()],
-  base: '/L2-Wildberries_to_do_app/',
+  base: './',
 });
