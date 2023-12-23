ce872681458c7e59467c068454f8650ae38e6556
ivan <ivanlisichkin@icloud.com>
2023-12-23 19:03:21 +0400

    fix:change sw

--- src/utils/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('push', function (event) {
-  const options = {
-    body: event.data.text(),
-  };
-
-  event.waitUntil(self.registration.showNotification('To Do App', options));
-});
